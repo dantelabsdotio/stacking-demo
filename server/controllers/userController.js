c637b675a9a9233156982ec3b17509210a0bd3bb
zacharyneipp <zacharyneipp@proton.me>
2025-08-10 14:34:52 -0300

    last

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ const sendToken = require('../utils/sendToken');
 const ErrorHandler = require('../utils/errorHandler');
 const sendEmail = require('../utils/sendEmail');
 const crypto = require('crypto');
-const cloudinary = require('cloud-binary');
+const cloudinary = require('cloudinary');
 
 // Register User
 exports.registerUser = asyncErrorHandler(async (req, res, next) => {
